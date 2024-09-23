732838d6e46c594120d23b449072e39124ab09f5
DaniArevalo <jarevaloc9@miumg.edu.gt>
2024-09-23 12:05:31 -0600

    Correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { CommonModule } from './common/common.module';
 import { ProductsModule } from './–no-spec/products/products.module';
 import { EvaluaciondosModule } from './–no-spec/evaluaciondos/evaluaciondos.module';
 import { ProductsModule } from './products/products.module';
+import { ServicesModule } from './services/services.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { EmpleadosModule } from './empleados/empleados.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [CommonModule, ProductsModule, EvaluaciondosModule],
+  imports: [CommonModule, ProductsModule, EvaluaciondosModule, ServicesModule, ClientesModule, EmpleadosModule],
   controllers: [AppController],
   providers: [AppService],
 })
